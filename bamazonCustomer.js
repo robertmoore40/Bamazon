cf8ce5a9e14d0bc5307332901c09ada777ee02db
Robert Moore <rcmoore3@ncsu.edu>
2019-11-16 19:13:41 -0500

    added inquirer, added number checks, number checks currently failing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,25 +2,55 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
+// var itemID = 
+// var item_Quantity = 
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	port: 3306,
 
-	user: 'root',
+// 	user: 'root',
 
-	password: 'Gokatie1',
-	database: 'bamazon_db'
-});
+// 	password: 'Gokatie1',
+// 	database: 'bamazon_db'
+// });
 
-connection.connect(function(err) {
-	if (err) throw err;
-	console.log("connected as id " + connection.threadId);
-	connection.end();
-  });
+// connection.connect(function(err) {
+// 	if (err) throw err;
+// 	console.log("connected as id " + connection.threadId);
+// 	connection.end();
+//   });
 
-function positiveCheck() {
-	  
+runBamazon();
+
+function validateIDCheck(input) {
+	// verify number is a counting number
+	if (input.item_ID > 0)  {
+		console.log("1/2 Number Validated")
+		return true;
+	} else {
+		return "Failed 1/2 Item ID Counting Number Check";
+	}	  
+};
+
+function validateQuantityCheck(input) {
+	// verify number is a counting number
+	if (input.item_Quantity > 0)  {
+		console.log("2/2 Number Validated")
+		return true;
+	} else {
+		return "Failed 2/2 Quantity Counting Number Check";
+	}	  
+};
+
+function validateCountingNumberCheck(input) {
+	// verify number is a counting number
+	if (input.item_Quantity && input.item_ID > 0)  {
+		// console.log("Numbers are valid")
+		return true;
+	} else {
+		return "Failed Double Check";
+	}	  
 };
 
 function inventoryDisplay() {
@@ -28,21 +58,60 @@ function inventoryDisplay() {
 };
 
 function customerInquirer() {
+inquirer.prompt([
+	{
+		type: 'input',
+		name: 'item_ID',
+		message: 'Enter the item ID',
+		// validate: validateIDCheck(),
+		validate: validateIDCheck,
+		filter: Number
+	},
+	{
+		type: 'input',
+		name: 'item_Quantity',
+		message: 'How many?',
+		// validate: validateQuantityCheck(),
+		// validate: validateQuantityCheck,
+		filter: Number
+	}
+]).then(function(input) {
+	console.log("Item ID chosen is " + input.item_ID);
+	console.log("Item quantity decided upon is " + input.item_Quantity);
+	console.log("got through!")
+	var itemID = input.item_ID;
+	var item_Quantity = input.item_Quantity;
+	quantityCheck();
 	  
-};
+})};
 
 function quantityCheck() {
+
+	// if quantity > sql quantity check
+	// run sufficient 
+	// else 
+	// run insufficientQuantity
 	  
 };
 
 function sufficientQuantity() {
-	  
+	//   console log success
+	// call order total calculation
+	// orderTotalCalculation();
+
 };
 
 function insufficientQuantity() {
-	  
+	//   Console log error, kick out user
 };
 
 function orderTotalCalculation() {
+	// log quantity * price
+	// kick user back out
 	  
+};
+
+function runBamazon () {
+	// inventoryDisplay();
+	customerInquirer();
 };
